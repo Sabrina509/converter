1c3770e04eb3337c6b68f77a4896ddad97262957
Sabrina Asim <sabrinaasim2000@gmail.com>
2021-10-31 18:58:40 +0500

    layout change

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -16,19 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button mybutton;
+        Button mybutton1;
+        Button mybutton2;
         final EditText fText;
+        final EditText fText1;
+        final EditText fText2;
         final TextView sText;
 
         mybutton= (Button) findViewById(R.id.button);
+        mybutton1= (Button) findViewById(R.id.button1);
+        mybutton2= (Button) findViewById(R.id.button2);
         fText=(EditText) findViewById(R.id.editText);
+        fText1=(EditText) findViewById(R.id.editText1);
+        fText2=(EditText) findViewById(R.id.editText2);
         sText=(TextView) findViewById(R.id.textView);
 
         mybutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double convert= Double.parseDouble(fText.getText().toString());
-                sText.setText(String.valueOf(convert*100));
-                sText.setTextColor(Color.RED);
+                if(fText==null && fText1==null) {
+                    Double convert = Double.parseDouble(fText2.getText().toString());
+                    sText.setText(String.valueOf(convert * 100));
+                    sText.setTextColor(Color.RED);
+                }
             }
         });
     }
