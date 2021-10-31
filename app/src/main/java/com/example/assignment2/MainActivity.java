588d9ccca681cdb9e5dad71b79adebe3d84e2418
Sabrina Asim <sabrinaasim2000@gmail.com>
2021-10-31 20:07:07 +0500

    code completed

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +18,72 @@ public class MainActivity extends AppCompatActivity {
         Button mbutton;
         Button cmbutton;
         Button kmbutton;
+
+
+        mbutton = (Button) findViewById(R.id.button);
+        cmbutton = (Button) findViewById(R.id.button1);
+        kmbutton = (Button) findViewById(R.id.button2);
+
+
+        mbutton.setOnClickListener((View.OnClickListener) this);
+        cmbutton.setOnClickListener((View.OnClickListener) this);
+        kmbutton.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
         final EditText kmText;
         final EditText cmText;
         final EditText mText;
         final TextView sText;
 
-        mbutton= (Button) findViewById(R.id.button);
-        cmbutton= (Button) findViewById(R.id.button1);
-        kmbutton= (Button) findViewById(R.id.button2);
-        mText=(EditText) findViewById(R.id.mText);
-        cmText=(EditText) findViewById(R.id.cmText);
-        kmText=(EditText) findViewById(R.id.kmText);
-        sText=(TextView) findViewById(R.id.textView);
+        mText = (EditText) findViewById(R.id.mText);
+        cmText = (EditText) findViewById(R.id.cmText);
+        kmText = (EditText) findViewById(R.id.kmText);
+        sText = (TextView) findViewById(R.id.textView);
 
-        mbutton.setOnClickListener(new View.OnClickListener() {
+        switch (v.getId()) {
+            case R.id.button:
+                if (kmText.getText().toString().length() == 0) {
+                    Double convert = Double.parseDouble(cmText.getText().toString());
+                    sText.setText(String.valueOf(convert / 100));
+                    sText.setTextColor(Color.RED);
+                } else if (cmText.getText().toString().length() == 0) {
+                    Double convert = Double.parseDouble(kmText.getText().toString());
+                    sText.setText(String.valueOf(convert * 1000));
+                    sText.setTextColor(Color.RED);
+                }
+                break;
+            case R.id.button1:
+                if (kmText.getText().toString().length() == 0) {
+                    Double convert = Double.parseDouble(mText.getText().toString());
+                    sText.setText(String.valueOf(convert * 100));
+                    sText.setTextColor(Color.RED);
+                } else if (mText.getText().toString().length() == 0) {
+                    Double convert = Double.parseDouble(kmText.getText().toString());
+                    sText.setText(String.valueOf(convert * 1000000));
+                    sText.setTextColor(Color.RED);
+                }
+                break;
+
+            case R.id.button2:
+                if (cmText.getText().toString().length() == 0) {
+                    Double convert = Double.parseDouble(mText.getText().toString());
+                    sText.setText(String.valueOf(convert / 1000));
+                    sText.setTextColor(Color.RED);
+                } else if (mText.getText().toString().length() == 0) {
+                    Double convert = Double.parseDouble(cmText.getText().toString());
+                    sText.setText(String.valueOf(convert / 1000000));
+                    sText.setTextColor(Color.RED);
+                }
+                break;
+        }
+    }
+}
+
+
+        /*mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(kmText==null) {
@@ -77,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
                     sText.setTextColor(Color.RED);
                 }
             }
-        });
-    }
-}
+        });*/
+
+
+
